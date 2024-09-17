dfd85a603252c055e8a795a52e75557814fe3ba4
Alexandr Petrov <arcanen94@gmail.com>
2024-09-17 23:09:06 +0500

    Added custom colors to MUI theme

--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -1,4 +1,5 @@
 'use client'
+import {ThemeOptions} from '@mui/material/styles'
 import {Roboto} from 'next/font/google'
 import {createTheme} from '@mui/material/styles'
 
@@ -8,10 +9,26 @@ const roboto = Roboto({
   display: 'swap',
 })
 
+const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: '#da6a00',
+    },
+    success: {
+      main: '#38b73e',
+    },
+  },
+}
+
 const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
   },
+  ...themeOptions,
 })
 
 export default theme
